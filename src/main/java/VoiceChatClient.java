3ec33ea64f868c3b8ae7a72323dd1ef2d6e00022
Angelo Ascione <goldrakealtarus@gmail.com>
2024-11-11 13:58:20 +0100

    First Commit

---
+++ src/main/java/VoiceChatClient.java
@@ -0,0 +1,37 @@
+import javax.sound.sampled.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class VoiceChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 50005;
+    private static final int BUFFER_SIZE = 2048;
+
+    public static void main(String[] args) {
+        try {
+            // Set up audio format for capturing
+            AudioFormat format = new AudioFormat(16000.0f, 16, 1, true, true);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
+            microphone.open(format);
+            microphone.start();
+
+            // Set up client socket
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getByName(SERVER_ADDRESS);
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            System.out.println("Voice Chat Client is sending audio...");
+
+            // Capture and send audio data
+            while (true) {
+                int bytesRead = microphone.read(buffer, 0, buffer.length);
+                DatagramPacket packet = new DatagramPacket(buffer, bytesRead, serverAddress, SERVER_PORT);
+                socket.send(packet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/VoiceChatServer.java
@@ -0,0 +1,34 @@
+import javax.sound.sampled.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class VoiceChatServer {
+    private static final int PORT = 50005;
+    private static final int BUFFER_SIZE = 2048;
+
+    public static void main(String[] args) {
+        try {
+            // Set up audio format for playback
+            AudioFormat format = new AudioFormat(16000.0f, 16, 1, true, true);
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+            SourceDataLine speaker = (SourceDataLine) AudioSystem.getLine(info);
+            speaker.open(format);
+            speaker.start();
+
+            // Set up server socket
+            DatagramSocket socket = new DatagramSocket(PORT);
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            System.out.println("Voice Chat Server is running...");
+
+            // Receive and play audio data
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                speaker.write(packet.getData(), 0, packet.getLength());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
